ef3baebc81b7de9ef33eff38785a17dbcbafe843
cmartins.ifpa <claudio.martins@ifpa.edu.br>
2021-07-01 20:07:34 -0300

    nova versão .... novidades ....

--- src/main/java/edu/ifpa/imc/controle/ImcController.java
+++ src/main/java/edu/ifpa/imc/controle/ImcController.java
@@ -23,7 +23,10 @@ public class ImcController {
 	@RequestMapping("/")
     @ResponseBody
     String home() {
-      return "<h1> Hello World! </h1>  <p> Esse e um codigo alterado para teste do CI</p>";
+      return "<h1> Hello World! </h1>"
+      		+ "  <p> Esse e um codigo alterado para teste do CI</p>"
+      		+ " <hr>  "
+      		+ "  TUDO FUNCIONANDO.....";
     }
 	
 	@RequestMapping(value = "/imc", method = RequestMethod.GET)
